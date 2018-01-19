20bf41bdd547d6607d689ffbde0384fc8520dbfa
Paweł Krakowiak <pwlkrakowiak@gmail.com>
2018-01-19 15:14:44 +0100

    initialized spring web mvc

---
+++ src/main/java/pl/coderslab/warsztat5/app/AppConfig.java
@@ -0,0 +1,13 @@
+package pl.coderslab.warsztat5.app;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"pl.coderslab.warsztat5.model", "pl.coderslab.warsztat5.controller"})
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+}
